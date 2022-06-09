378c492ade181d80db199deeddfa1f49fd0777db
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-06-10 00:42:25 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(records){
+    const denverYear = records.find(winningSeason);
+    if (!!denverYear === true){
+        return denverYear.year
+    }
+    else {
+        return undefined
+    }
+    
+}
+
+const winningSeason = function(record) {
+    return record.result === 'W'  
+}
